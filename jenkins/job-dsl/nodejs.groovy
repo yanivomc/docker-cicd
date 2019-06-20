2ebae708acb5e1a8baad64f9f2e50d790fa337f4
yanivomc-k1 <yanivomc@gmail.com>
2019-06-20 14:50:45 +0300

    Updated

--- jenkins/job-dsl/nodejs.groovy
+++ jenkins/job-dsl/nodejs.groovy
@@ -17,3 +17,28 @@ job('NodeJS example') {
     }
 }
 
+job('NodeJS Docker example') {
+    scm {
+        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs-new') 
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('yanivomc/docker-nodejs-demo') //qa / dev
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
