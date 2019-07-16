a31859bcbf997133fc7a070be964e9c5aef05a19
yanivomc-k1 <yanivomc@gmail.com>
2019-07-16 16:42:00 +0300

    added .net application

---
+++ AccountOwnerServer/Startup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using AccountOwnerServer.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using System.IO;
+using NLog.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace AccountOwnerServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.ConfigureNLog(String.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.ConfigureCors();
+
+            services.ConfigureIISIntegration();
+
+            services.ConfigureLoggerService();
+
+            services.ConfigureMySqlContext(Configuration);
+
+            services.ConfigureRepositoryWrapper();
+
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "AccountOwner API", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseCors("CorsPolicy");
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+                if (context.Response.StatusCode == 404
+                    && !Path.HasExtension(context.Request.Path.Value))
+                {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+            });
+
+            app.UseStaticFiles();
+
+            app.UseMvc();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "AccountOwner API V1");
+            });
+        }
+    }
+}
---
+++ Repository/AccountRepository.cs
@@ -0,0 +1,30 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Repository
+{
+    public class AccountRepository: RepositoryBase<Account>, IAccountRepository
+    {
+        public AccountRepository(RepositoryContext repositoryContext)
+            :base(repositoryContext)
+        {
+        }
+
+        public IEnumerable<Account> GetAllAccounts()
+        {
+            return FindAll()
+                .OrderBy(ac => ac.DateCreated);
+        }
+
+        public void CreateAccount(Account account)
+        {
+            account.Id = Guid.NewGuid();
+            Create(account);
+            Save();
+        }
+    }
+}
