ca0be48987f97f77de5bf32f3289fa9589e8140d
yanivomc-k1 <yanivomc@gmail.com>
2020-01-15 15:41:18 +0200

    stamd more job

--- basics/jenkins/job-dsl/nodejs_docker.groovy
+++ basics/jenkins/job-dsl/nodejs_docker.groovy
@@ -23,3 +23,29 @@ job('NodeJS Docker example') {
         }
     }
 }
+
+job('stam Docker example') {
+    scm {
+        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs-new') 
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('aaaaaaaaa/docker-nodejs-demo')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
