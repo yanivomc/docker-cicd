0876876d3ce12e8021c4d655eca6d0fbf00a2eff
yanivomc <yanivomc@gmail.com>
2021-07-06 15:42:37 +0300

    Update index.js

--- basics/index.js
+++ basics/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello this is my class!');
+  res.send('Hello this is my super class!');
 });
 
 var server = app.listen(3000, function () {
