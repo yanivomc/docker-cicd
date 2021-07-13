2b38b570bb90a982a75377d22431e6284ef68797
yanivomc <yanivomc@gmail.com>
2021-07-13 11:51:21 +0300

    Create nodejs-docker.groovy

---
+++ basics/dsl/nodejs-docker.groovy
@@ -0,0 +1,26 @@
+job('NodeJS Docker example') {
+    scm {
+        git('git://github.com/yanivomc/docker-cicd.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@devophift.work')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+   
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName(yanivomc/amdocs-app)
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('yaniv-dockerhub')
+            buildContext('./basics/'
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
+
