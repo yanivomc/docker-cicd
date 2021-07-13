46a170045bdbf736362ec025e3492694a1d4edf4
yanivomc <yanivomc@gmail.com>
2021-07-13 10:21:54 +0300

    Update nodejs.groovy

--- basics/dsl/nodejs.groovy
+++ basics/dsl/nodejs.groovy
@@ -1,7 +1,7 @@
 job('NodeJS example') {
     scm {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
+            node / gitConfigName('DSL yaniv')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
