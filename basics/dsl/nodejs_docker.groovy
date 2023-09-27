10f27ac088a374f4a24892d9f988f24ead5ed6da
Aviad48 <aviaddayan1991@gmail.com>
2023-09-27 21:36:34 +0300

    Update nodejs_docker.groovy

--- basics/dsl/nodejs_docker.groovy
+++ basics/dsl/nodejs_docker.groovy
@@ -1,6 +1,6 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/yanivomc/docker-cicd.git','master') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/yanivomc/docker-cicd.git','master') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@devophift.work')
         }
@@ -12,9 +12,9 @@ job('NodeJS Docker example') {
     
     steps {
         dockerBuildAndPublish {
-            repositoryName('yanivomc/amdocsapp')
+            repositoryName('aviad539/amdocsapp')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('yaniv-dockerhub')
+            registryCredentials('769ab113-cc97-4c17-9c90-55e2b40b8031')
             buildContext('./basics/')
             forcePull(false)
             forceTag(false)
