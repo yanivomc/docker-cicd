cff40c3aa78a59a854a03d13a500fbdea2420d02
root <admin@example.com>
2023-11-07 16:44:45 +0200

    update

--- dsl/boilerplateJobs.groovy
+++ dsl/boilerplateJobs.groovy
@@ -1,7 +1,7 @@
 // Define job internal variables
 def gitUrlBuildroot = 'http://gitlab.satixfy.lan/sw_host/buildroot.git'
 def gitUrlUboot = 'http://gitlab.satixfy.lan/sw_host/u-boot.git'
-def gitUrlKernel = 'http://gitlab.satixfy.lan/sw_host/kernel.git'
+def gitUrlKernel = 'http://gitlab.satixfy.lan/sw_host/linux.git'
 def gitCredentialsId = 'git-credentials-id' // Replace with your actual Jenkins credentials ID
 
 
