67f8330b327e7c0ce6a208ec66719fdeb6819398
root <admin@example.com>
2023-11-07 16:39:19 +0200

    added kernel

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 docker build  -t satixfy-repo.devopshift.com/buildroot/buildroot-cicd:1.00.0 -f buildroot.cicd.Dockerfile . 
 docker build  -t satixfy-repo.devopshift.com/u-boot/u-boot-cicd:1.00.0 -f u-boot.cicd.Dockerfile .
+docker push satixfy-repo.devopshift.com/u-boot/u-boot-cicd:1.00.0
+
+
+docker build  -t satixfy-repo.devopshift.com/kernel/kernel-cicd:1.00.0 -f kernel.cicd.Dockerfile .
+docker push satixfy-repo.devopshift.com/kernel/kernel-cicd:1.00.0
--- dsl/boilerplateJobs.groovy
+++ dsl/boilerplateJobs.groovy
@@ -1,6 +1,7 @@
 // Define job internal variables
 def gitUrlBuildroot = 'http://gitlab.satixfy.lan/sw_host/buildroot.git'
 def gitUrlUboot = 'http://gitlab.satixfy.lan/sw_host/u-boot.git'
+def gitUrlKernel = 'http://gitlab.satixfy.lan/sw_host/kernel.git'
 def gitCredentialsId = 'git-credentials-id' // Replace with your actual Jenkins credentials ID
 
 
@@ -34,7 +35,28 @@ pipelineJob('u-boot') {
                         credentials(gitCredentialsId) // Replace with your actual Jenkins credentials ID
                     }
                     branches('jenkins_docker')
-                    scriptPath('u-boot.pipeline')
+                    scriptPath('/buildroot/u-boot.pipeline')
+                }
+            }
+        }
+    }
+    triggers {
+        scm('H/5 * * * *') // Triggers the build when a change is detected in the repository every 5 minutes
+    }
+}
+
+
+pipelineJob('kernel') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url(gitUrlKernel)
+                        credentials(gitCredentialsId) // Replace with your actual Jenkins credentials ID
+                    }
+                    branches('jenkins_docker')
+                    scriptPath('/buildroot/kernel.pipeline')
                 }
             }
         }
