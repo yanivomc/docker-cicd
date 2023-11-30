24d6c421c58386ad196b29944f69e8b4c6f62869
root <admin@example.com>
2023-11-30 09:30:08 +0200

    updates

--- jenkins/automation/binary-manager/docker-based/src/uploader.py
+++ jenkins/automation/binary-manager/docker-based/src/uploader.py
@@ -1,6 +1,14 @@
+# docker run -ti  -v "$(pwd)":/resources/ --env FILE_PATH=/resources/entrypoint.sh --env NEXUS_REPO_URL='https://satixfy-repo.devopshift.com/nexus/repository/satixfy-raw-repo' --env NEXUS_USER=admin --env NEXUS_PASSWORD=admin satixfy-repo.devopshift.com/binary_manager/binary_manager:1.00.1
 import os
 import requests
 from requests.auth import HTTPBasicAuth
+
+def usage():
+    print("---------------------------------------------")
+    print(f"The following environment variables must be set: FILE_PATH, NEXUS_REPO_PROJECT, NEXUS_USER, NEXUS_PASSWORD")
+    print(f"Optional: NEXUS_BASE_URL (default: https://satixfy-repo.devopshift.com/nexus/repository/satixfy-raw-repo)")
+    print(f"Example: docker run -ti -v \"$(pwd)\":/resources/ --env FILE_PATH=/resources/entrypoint.sh --env NEXUS_REPO_PROJECT=u-boot  --env NEXUS_USER=admin --env NEXUS_PASSWORD=adminpass satixfy-repo.devopshift.com/binary_manager/binary_manager:1.00.1")
+    return
 
 def upload_to_nexus(file_path, nexus_repo_url, nexus_user, nexus_password):
     """
@@ -19,6 +27,7 @@ def upload_to_nexus(file_path, nexus_repo_url, nexus_user, nexus_password):
         upload_url = f"{nexus_repo_url}/{file_path.split('/')[-1]}"
 
         # Make the PUT request to upload the file
+        print(f"Uploading {file_path} to {upload_url}...")
         response = requests.put(upload_url, data=file_to_upload, auth=HTTPBasicAuth(nexus_user, nexus_password))
 
         # Check if the upload was successful
@@ -30,21 +39,33 @@ def upload_to_nexus(file_path, nexus_repo_url, nexus_user, nexus_password):
 # Grab configuration from os environment variables
 file_path = os.getenv('FILE_PATH', '')
 # ex: 'http://your-nexus-server/repository/your-repo'
-nexus_repo_url = os.getenv('NEXUS_REPO_URL', '')
+nexus_base_url = os.getenv('NEXUS_BASE_URL', 'https://satixfy-repo.devopshift.com/nexus/repository/satixfy-raw-repo')
+nexus_repo_project = os.getenv('NEXUS_REPO_PROJECT', '')
+nexus_repo_url = f"{nexus_base_url}/{nexus_repo_project}"
+# DEBUG: 
+# print(f"nexus_repo_url: {nexus_repo_url}")
 nexus_user = os.getenv('NEXUS_USER', '')
 nexus_password = os.getenv('NEXUS_PASSWORD', '')
 # Vlidate that all variables are set and not empty strings and let the user know if they are not set correctly
-if file_path == '':
-    print("FILE_PATH environment variable not set.")
+if file_path == '' or nexus_base_url == '' or nexus_repo_project == '' or nexus_user == '' or nexus_password == '':
+    print("ERROR: Environment variables are not set or missing.")
+    usage()
     raise SystemExit(1)
-elif  nexus_repo_url == '':
-    print("NEXUS_REPO_URL environment variable not set.")
-    raise SystemExit(1)
-elif  nexus_user == '':
-    print("NEXUS_USER environment variable not set.")
-    raise SystemExit(1)
-elif  nexus_password == '':
-    print("NEXUS_PASSWORD environment variable not set.")
-    raise SystemExit(1)
+# elif  nexus_base_url == '':
+#     print("NEXUS_BASE_URL environment variable not set.")
+#     usage()
+#     raise SystemExit(1)
+# elif  nexus_repo_project == '':
+#     print("NEXUS_REPO_PROJECT environment variable not set.")
+#     usage()
+#     raise SystemExit(1)
+# elif  nexus_user == '':
+#     print("NEXUS_USER environment variable not set.")
+#     usage()
+#     raise SystemExit(1)
+# elif  nexus_password == '':
+#     print("NEXUS_PASSWORD environment variable not set.")
+#     usage()
+#     raise SystemExit(1)
 else:
     upload_to_nexus(file_path, nexus_repo_url, nexus_user, nexus_password)
